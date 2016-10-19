22261a0dd9d857a345963d14aff188ee579d74d6
stu <stu@qq.com>
2016-10-19 11:12:31 +0800

    添加新闻列表功能

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace News.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 欢迎页面 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Welcome()
         {
             var a = 100;
@@ -30,6 +34,24 @@ namespace News.Controllers
             }
 
             ViewBag.sum = sum;
+            return View();
+        }
+
+
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            string[] data= new string[] { "台风逼近广西景区关闭学校停课",
+                "广西每年入境游客超300万" ,
+                "空军招飞初选10月24日开始",
+                "柳州城市轨道交通勘探工作已展开"};
+
+            //ViewBag.data = data;
+            //ViewData["data"] = data;
+            ViewData.Model = data;
             return View();
         }
 
